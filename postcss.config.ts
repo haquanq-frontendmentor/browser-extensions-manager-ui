d2037dacf2f962f08966dd7bcd90d896373cdcfb
Ha Quanq <quangha0120010@gmail.com>
2025-07-02 13:10:33 +0700

    Add dependencies and configs

---
+++ postcss.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+export default {
+    plugins: {
+        "postcss-nested": {},
+        "postcss-import": {},
+        "postcss-preset-env": {},
+        autoprefixer: {},
+        cssnano: {},
+    },
+};
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { UserConfig } from "vite";
+
+export default {
+    root: "src/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/ts"),
+            "@components": path.resolve(__dirname, "./src/ts/components"),
+            "@stores": path.resolve(__dirname, "./src/ts/stores"),
+            "@utils": path.resolve(__dirname, "./src/ts/utils"),
+            "@@types": path.resolve(__dirname, "./src/ts/types"),
+        },
+    },
+    test: {
+        root: "./",
+        environment: "jsdom",
+        coverage: {
+            enabled: true,
+            provider: "v8",
+        },
+        reporters: "verbose",
+        css: true,
+    },
+} satisfies UserConfig;
